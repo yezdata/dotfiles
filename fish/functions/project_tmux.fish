2ad5028a98a2e32cc32b3f40016a5f6122ebfabe
Václav Jež <jez.vasek@gmail.com>
2026-06-11 23:43:17 +0200

    add uv venv activation to tmux project jumping script

--- fish/functions/project_tmux.fish
+++ fish/functions/project_tmux.fish
@@ -8,9 +8,11 @@ function project_tmux --description "Open project from ~/projects in tmuxu with 
     set -l session_name (basename $target_dir | string replace -a '.' '_')
 
     if not tmux has-session -t $session_name 2>/dev/null
-        tmux new-session -d -s $session_name -n "nvim" -c $target_dir "nvim"
+        set -l venv_cmd "test -f .venv/bin/activate.fish && source .venv/bin/activate.fish"
+
+        tmux new-session -d -s $session_name -n "nvim" -c $target_dir "fish -i -c 'test -f .venv/bin/activate.fish && source .venv/bin/activate.fish; nvim; exec fish'"
         
-        tmux new-window -t $session_name:2 -n "shell" -c $target_dir
+        tmux new-window -t $session_name:2 -n "shell" -c $target_dir "fish -i -c 'test -f .venv/bin/activate.fish && source .venv/bin/activate.fish; exec fish'"
         
         tmux select-window -t $session_name:1
     end
