09a63573c78896024b65ee5edcc40affc3e810e1
Václav Jež <jez.vasek@gmail.com>
2026-07-11 18:49:24 +0200

    add new tree-sitter manager instead of deprecetad nvim-treesitter

---
+++ nvim/lua/plugins/tree-sitter.lua
@@ -0,0 +1,10 @@
+return {
+    "romus204/tree-sitter-manager.nvim",
+    dependencies = {}, -- tree-sitter CLI must be installed system-wide
+    config = function()
+        require("tree-sitter-manager").setup({
+            auto_install = false,
+            ensure_installed = { "python" },
+        })
+    end,
+}
